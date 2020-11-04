f7baa234af94e28975f021cfa538697271cd656c
AnastasiaIQA12 <Sinka8900@mail.ru>
2020-11-04 12:29:04 +0300

    Change project

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,7 @@
 public class BmiService {
-    public float calculate (int weight_in_kg, int height_in_sm){
-       float height = (float) height_in_sm/100;
-       float body_mass_index = (float) weight_in_kg / (height*height);
-       return body_mass_index;
+    public float calculate(int WeightInKg, int HeightInSm) {
+        float height = (float) HeightInSm / 100;
+        float BodyMassIndex = (float) WeightInKg / (height * height);
+        return BodyMassIndex;
     }
 }
